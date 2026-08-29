09c9581a5f8e1b24eaecb7324c550f90200bdcf6
obaodelana <obaloluwa.dev@gmail.com>
2026-08-29 00:42:09 -0600

    chore: add TWILIO_SEND_URL config and rename terraform project

--- src/agent/deployment/terraform/single-project/variables.tf
+++ src/agent/deployment/terraform/single-project/variables.tf
@@ -15,7 +15,7 @@
 variable "project_name" {
   type        = string
   description = "Project name used as a base for resource naming"
-  default     = "agent"
+  default     = "classy"
 }
 
 variable "project_id" {
